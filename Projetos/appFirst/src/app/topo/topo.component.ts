c2bdbb6156063c5b9ca45dab1c88a7975c77f9f5
Luis Antônio de Melo Gomes <tyluis20@gmail.com>
2018-08-16 13:18:39 -0300

    app angular

---
+++ Projetos/appFirst/src/app/topo/topo.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core'
+
+@Component({
+    selector:'app-topo',
+    templateUrl:'./topo.component.html',
+   // template: `<p> Essse é um component</p>`
+   styleUrls: ['./topo.component.css']
+})
+
+export class TopoComponent {
+
+}
